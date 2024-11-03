a8151587a24fd33858ca9ec32530c6896237825f
DAGribanov <DAGribanov@nota.tech>
2024-11-03 14:35:29 +0500

    feat: add swagger documentation

--- src/users/dto/update-user-password-dto.ts
+++ src/users/dto/update-user-password-dto.ts
@@ -1,4 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class UpdateUserPasswordDto {
+  @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
   readonly id: number;
+  @ApiProperty({example: 'qwerty', description: 'Пароль'})
   readonly password: string;
 }
--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,4 +1,5 @@
 import { Column, DataType, Model, Table } from 'sequelize-typescript';
+import { ApiProperty } from '@nestjs/swagger';
 
 interface UserCreationAttrs {
   email: string;
@@ -7,12 +8,15 @@ interface UserCreationAttrs {
 
 @Table({ tableName: 'users' })
 export class User extends Model<User, UserCreationAttrs> {
+  @ApiProperty({example: '1', description: 'Уникальный идентификатор'})
   @Column({ type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true })
   id: number;
 
+  @ApiProperty({example: 'user@example.com', description: 'Адрес электронной почты'})
   @Column({ type: DataType.STRING, unique: true, allowNull: false })
   email: string;
 
+  @ApiProperty({example: 'qwerty', description: 'Пароль'})
   @Column({ type: DataType.STRING, allowNull: false })
   password: string;
 }
